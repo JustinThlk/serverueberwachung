8cf9ddec416281931d4b3c935b7544f50a310697
Jan Arning <J.Arning@deos-ag.com>
2023-05-02 14:59:13 +0200

    Added Eastereggs and status tabula with tabulate

--- serverueberwachung/CommandHandler.py
+++ serverueberwachung/CommandHandler.py
@@ -1,5 +1,6 @@
 from Bricklets import Bricklets
 from DataListener import DataListener
+from tabulate import tabulate
 import resources as resc
 
 class CommandHandler:
@@ -16,6 +17,10 @@ class CommandHandler:
             print("Hilfe ist unterwegs")
         elif command == "open the pod bay doors hal":
             print("I'm sorry, Dave. I'm afraid I can't do that.")
+        elif command == "beam me up scotty":
+            print("Roger Roger")
+        elif command == ("luke i am your father" or "i am your father"):
+            print("No, it can't be!")
 
         # Commands for temperature_sensor:
         elif command == "get_temp":
@@ -30,13 +35,20 @@ class CommandHandler:
         elif command == "get_light":
             print(str(self.Bricklets.LightSensor.get_illuminance()/100.0) + " lx")
         elif command == "status":
-            print(str(self.Bricklets.TemperatureSensor.get_temperature() / 100.0) + " Grad Celsius\n")
-            print(str(self.Bricklets.HumiditySensor.get_humidity() / 100.0) + "% Luftfeuchtigkeit\n")
+            temp_temperature = self.Bricklets.TemperatureSensor.get_temperature() / 100.0
+            temp_humidity = self.Bricklets.HumiditySensor.get_humidity() / 100.0
             if self.Bricklets.MotionDetector.get_motion_detected() == 1:
-                print("Bewegung erkannt\n")
+                temp_motion = "Bewegung erkannt"
             else:
-                print("Keine Bewegung erkannt\n")
-            print(str(self.Bricklets.LightSensor.get_illuminance() / 100.0) + " lx")
+                temp_motion = "Keine Bewegung erkannt"
+            temp_light = self.Bricklets.LightSensor.get_illuminance() / 100.0
+            # Used tabulate for this: https://pypi.org/project/tabulate/
+            print(tabulate([['Temperatur', temp_temperature],
+                            ['Feuchtigkeit', temp_humidity],
+                            ['Bewegungsmelder', temp_motion],
+                            ['Helligkeit', temp_light]],
+                           headers=['Sensor', 'Wert'],
+                           tablefmt="rounded_outline"))
         elif command == "set_colour":
             R_value = input("Wert für Rot(0-255): ")
             G_value = input("Wert für Grün(0-255): ")
