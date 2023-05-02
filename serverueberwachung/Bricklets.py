65c45bff9ee0b5c62347d791e6958cf890111ddc
JustinThlk <86998607+JustinThlk@users.noreply.github.com>
2023-05-02 12:45:17 +0200

    Changes

--- serverueberwachung/Bricklets.py
+++ serverueberwachung/Bricklets.py
@@ -1,10 +1,19 @@
 from tinkerforge.ip_connection import IPConnection
 
 import resources as resc
+
+#
+# References of all sensors
 from tinkerforge.bricklet_ptc_v2 import BrickletPTCV2
 from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
 from tinkerforge.bricklet_ambient_light_v3 import BrickletAmbientLightV3
 from tinkerforge.bricklet_humidity_v2 import BrickletHumidityV2
+# References to all actors
+from tinkerforge.bricklet_rgb_led_button import BrickletRGBLEDButton
+from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
+from tinkerforge.bricklet_piezo_speaker_v2 import PiezoSpeakerV2
+from tinkerforge.bricklet_segment_display_4x7_v2 import SegmentDisplay4x7V2
+from tinkerforge.bricklet_e_paper_296x128 import EPaper296x128
 
 class Bricklets:
     def __init__(self):
@@ -12,6 +21,7 @@ class Bricklets:
         self.TemperatureSensor = BrickletPTCV2(resc.uid_list["tempsensor"], ipcon)
         self.MotionDetector = BrickletMotionDetectorV2(resc.uid_list["motiondetector"], ipcon)
         self.LightSensor = BrickletAmbientLightV3(resc.uid_list["ambientlight"], ipcon)
-        # self.HumiditySensor = BrickletHumidityV2(resc.uid_list["humidity"], resc.ipcon_Humidity)
+        self.HumiditySensor = BrickletHumidityV2(resc.uid_list["humidity"], ipcon)
+        self.RGBButton = BrickletRGBLEDButton(resc.uid_list["rgbledbutton"], ipcon)
         ipcon.connect(resc.HOST, resc.PORT)
         # Don't use device before ipcon is connected
--- serverueberwachung/CommandHandler.py
+++ serverueberwachung/CommandHandler.py
@@ -14,9 +14,33 @@ class CommandHandler:
         #Basic Commands
         if command == "help":
             print("Hilfe ist unterwegs")
+        elif command == "open the pod bay doors hal":
+            print("I'm sorry, Dave. I'm afraid I can't do that.")
 
         # Commands for temperature_sensor:
         elif command == "get_temp":
             print(str(self.Bricklets.TemperatureSensor.get_temperature()/100.0) + " Grad Celsius\n")
+        elif command == "get_humidity":
+            print(str(str(self.Bricklets.HumiditySensor.get_humidity()/100.0) + "% Luftfeuchtigkeit\n"))
+        elif command == "detect_motion":
+            if self.Bricklets.MotionDetector.get_motion_detected() == 1:
+                print("Bewegung erkannt\n")
+            else:
+                print("Keine Bewegung erkannt\n")
+        elif command == "get_light":
+            print(str(self.Bricklets.LightSensor.get_illuminance()/100.0) + " lx")
+        elif command == "status":
+            print(str(self.Bricklets.TemperatureSensor.get_temperature() / 100.0) + " Grad Celsius\n")
+            print(str(self.Bricklets.HumiditySensor.get_humidity() / 100.0) + "% Luftfeuchtigkeit\n")
+            if self.Bricklets.MotionDetector.get_motion_detected() == 1:
+                print("Bewegung erkannt\n")
+            else:
+                print("Keine Bewegung erkannt\n")
+            print(str(self.Bricklets.LightSensor.get_illuminance() / 100.0) + " lx")
+        elif command == "set_colour":
+            R_value = input("Wert für Rot(0-255): ")
+            G_value = input("Wert für Grün(0-255): ")
+            B_value = input("Wert für Blau(0-255): ")
+            self.Bricklets.RGBButton.set_color(R_value, G_value, B_value)
         else:
             return "Befehl unbekannt\n"
